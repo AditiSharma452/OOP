92c4f86c619cd08bb2a0b09d0275f84911978792
Aditi Sharma <aditi.sharma04052002@gmail.com>
2024-06-11 01:08:50 +0530

    OOP2

---
+++ src/com/aditi/packages/a/Greetings.java
@@ -0,0 +1,8 @@
+package com.aditi.packages.a;
+import static com.aditi.packages.b.Message.message;
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        message();
+    }
+}
